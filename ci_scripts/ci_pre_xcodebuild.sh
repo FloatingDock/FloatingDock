d629255cab4aa8f69e996202a6874d0031d899d5
Thomas Bonk <thomas@meandmymac.de>
2023-11-23 19:22:23 +0100

    added output of info-plist file

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+/usr/libexec/PlistBuddy -c 'Print' FloatingDock/Info.plist
+
 # Steps:
 # 1. Read CUSTOM_MARKETING_VERSION from Info-plist file
 customMarketingVersion=`/usr/libexec/PlistBuddy -c 'Print :CUSTOM_MARKETING_VERSION' FloatingDock/Info.plist`
+
 # 2. Calculate version: ${CUSTOM_MARKETING_VERSION}.0.${BUILD_NUMBER}
 version="${customMarketingVersion}.0.${BUILD_NUMBER}"
+
 # 3. Set CFBundleShortVersionString in Info.plist to calculated version
 /usr/libexec/PlistBuddy -c 'Set :CFBundleShortVersionString ${version}' FloatingDock/Info.plist
