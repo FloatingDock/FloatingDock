b3dcac35ea1e0eddade9a4f385770ec233bc3a1f
Thomas Bonk <thomas@meandmymac.de>
2023-11-23 19:31:07 +0100

    ficex repository path

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -2,16 +2,16 @@
 
 echo "*****PWD*****"
 pwd
-echo "******$CI_PRIMARY_REPOSITORY*****"
+echo "******$CI_PRIMARY_REPOSITORY_PATH*****"
 
-/usr/libexec/PlistBuddy -c 'Print' $CI_PRIMARY_REPOSITORY/FloatingDock/Info.plist
+/usr/libexec/PlistBuddy -c 'Print' $CI_PRIMARY_REPOSITORY_PATH/FloatingDock/Info.plist
 
 # Steps:
 # 1. Read CUSTOM_MARKETING_VERSION from Info-plist file
-customMarketingVersion=`/usr/libexec/PlistBuddy -c 'Print :CUSTOM_MARKETING_VERSION' $CI_PRIMARY_REPOSITORY/FloatingDock/Info.plist`
+customMarketingVersion=`/usr/libexec/PlistBuddy -c 'Print :CUSTOM_MARKETING_VERSION' $CI_PRIMARY_REPOSITORY_PATH/FloatingDock/Info.plist`
 
 # 2. Calculate version: ${CUSTOM_MARKETING_VERSION}.0.${BUILD_NUMBER}
 version="$customMarketingVersion.0.$BUILD_NUMBER"
 
 # 3. Set CFBundleShortVersionString in Info.plist to calculated version
-/usr/libexec/PlistBuddy -c 'Set :CFBundleShortVersionString ${version}' $CI_PRIMARY_REPOSITORY/FloatingDock/Info.plist
+/usr/libexec/PlistBuddy -c 'Set :CFBundleShortVersionString ${version}' $CI_PRIMARY_REPOSITORY_PATH/FloatingDock/Info.plist
