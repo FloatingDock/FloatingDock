132754f31733b069b000e35ae9c901488b0c9f98
Thomas Bonk <thomas@meandmymac.de>
2023-11-23 19:28:02 +0100

    added debug output

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-/usr/libexec/PlistBuddy -c 'Print' FloatingDock/Info.plist
+echo "*****PWD*****"
+pwd
+echo "******$CI_PRIMARY_REPOSITORY*****"
+
+/usr/libexec/PlistBuddy -c 'Print' $CI_PRIMARY_REPOSITORY/FloatingDock/Info.plist
 
 # Steps:
 # 1. Read CUSTOM_MARKETING_VERSION from Info-plist file
-customMarketingVersion=`/usr/libexec/PlistBuddy -c 'Print :CUSTOM_MARKETING_VERSION' FloatingDock/Info.plist`
+customMarketingVersion=`/usr/libexec/PlistBuddy -c 'Print :CUSTOM_MARKETING_VERSION' $CI_PRIMARY_REPOSITORY/FloatingDock/Info.plist`
 
 # 2. Calculate version: ${CUSTOM_MARKETING_VERSION}.0.${BUILD_NUMBER}
-version="${customMarketingVersion}.0.${BUILD_NUMBER}"
+version="$customMarketingVersion.0.$BUILD_NUMBER"
 
 # 3. Set CFBundleShortVersionString in Info.plist to calculated version
-/usr/libexec/PlistBuddy -c 'Set :CFBundleShortVersionString ${version}' FloatingDock/Info.plist
+/usr/libexec/PlistBuddy -c 'Set :CFBundleShortVersionString ${version}' $CI_PRIMARY_REPOSITORY/FloatingDock/Info.plist
